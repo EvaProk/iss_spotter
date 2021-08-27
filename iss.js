aa0b1272639deb5b405819ed9fe3af249e8a273d
Eva Prokhorenko <evaprokhorenko@gmail.com>
2021-08-27 02:26:48 +0000

    create index.js iss.js

---
+++ iss.js
@@ -0,0 +1,106 @@
+const request = require('request');
+
+
+const fetchMyIP = function(callback) {
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const data = JSON.parse(body).ip;
+
+    callback(null, data);
+  });
+};
+
+const fetchCoordsByIP = function(ip,callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+  
+    const coordinate = {latitude: JSON.parse(body).latitude, longitude: JSON.parse(body).longitude};
+
+    callback(null, coordinate);
+  });
+
+};
+
+
+const fetchISSFlyOverTimes = function(coords, callback) {
+
+  request(`http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const flyOver = JSON.parse(body).response;
+  
+
+    callback(null, flyOver);
+  });
+  
+};
+
+/**
+ * Orchestrates multiple API requests in order to determine the next 5 upcoming ISS fly overs for the user's current location.
+ * Input:
+ *   - A callback with an error or results.
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly-over times as an array (null if error):
+ *     [ { risetime: <number>, duration: <number> }, ... ]
+ */
+const nextISSTimesForMyLocation = function(callback) {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    
+    fetchCoordsByIP(ip, (error, coordinates) => {
+      if (error) {
+        callback(error, null);
+        return;
+      }
+
+      fetchISSFlyOverTimes(coordinates,(error, pass) => {
+        if (error) {
+          callback(error, null);
+          return;
+        }
+      
+        callback(null, pass);
+      });
+    });
+  });
+  
+};
+
+
+
+module.exports = {
+  nextISSTimesForMyLocation
+};
